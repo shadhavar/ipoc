b2006b915f0b375c4c9bc7a5ae05377cd5440268
biertie <bert@devnox.be>
2012-07-30 19:34:07 +0200

    tabs to spances, and removed some back up files

--- sender/sender.py
+++ sender/sender.py
@@ -14,32 +14,32 @@ log = {}
 
 def start():
     if os.path.exists('test'):
-	try:
-	    f = open('test')
-	except:
-	    print 'Error opening file'
-	old = json.loads(f.readline())
-	for entry in log:
-	    #are the files changed?
-	    if log[entry]['partition'] == old[entry]['partition'] and log[entry]['inode'] == old[entry]['inode']:
-		#if updates already have happened since last check, update offset so everything will be loaded
-		if log[entry]['offset'] != old[entry]['offset']:
-		    log[entry]['offset'] = old[entry]['offset']
-	    else:
-		    log[entry]['offset'] = 0
+        try:
+            f = open('test')
+        except:
+            print 'Error opening file'
+        old = json.loads(f.readline())
+        for entry in log:
+            #are the files changed?
+            if log[entry]['partition'] == old[entry]['partition'] and log[entry]['inode'] == old[entry]['inode']:
+                #if updates already have happened since last check, update offset so everything will be loaded
+                if log[entry]['offset'] != old[entry]['offset']:
+                    log[entry]['offset'] = old[entry]['offset']
+            else:
+                    log[entry]['offset'] = 0
 
 
 def whatlogs():
     if os.path.exists('logfiles'):
-	logs = list()
+        logs = list()
         try:
             f = open('logfiles', 'r')
-	except:
-	    print 'bar'
+        except:
+            print 'bar'
         for row in f.readlines():
-	    rowSplit = string.split(row, ' ', 2)
-	    logs.append(rowSplit)
-	return logs
+            rowSplit = string.split(row, ' ', 2)
+            logs.append(rowSplit)
+        return logs
 
 def findlogs(pointer, offset=-1):
     if offset != -1:
@@ -57,17 +57,17 @@ def watch():
     i=0
 
     for logfile in logs:
-	filename = logfile[1][:-1]
+        filename = logfile[1][:-1]
         pointers[i][1] = open(filename, 'r')
         st = os.stat(filename)
-	i+=1
-	log[logfile[0]] = {'inode': st.st_ino, 'partition': st.st_dev, 'offset': st.st_size}
+        i+=1
+        log[logfile[0]] = {'inode': st.st_ino, 'partition': st.st_dev, 'offset': st.st_size}
 
     start()
 
     while True:
-	for pointer in pointers:
-	    for line in findlogs(pointer, int(log[pointer[0]]['offset'])):
+        for pointer in pointers:
+            for line in findlogs(pointer, int(log[pointer[0]]['offset'])):
                 yield (pointer[0], line)
 
 try:
