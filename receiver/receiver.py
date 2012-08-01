16ea26900bedb036d94322b4ddf07b0cefa94072
biertie <bert@devnox.be>
2012-08-01 19:19:23 +0200

    changed the receiver, now it takes data out of the security messages

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -2,8 +2,7 @@
 import datetime
 import zmq
 import string
-
-
+import re
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
@@ -38,26 +37,81 @@ def getDetails(dirtyString):
     offset, time = getTimestamp(offset, dirtyString)
     return time, hostname, dirtyString[offset+2:].strip()
 
+def extractSecurity(message):
+    warning = -1
+    process = -1
+    login = -1
+    uid = -1
+    euid = -1
+    tty = -1
+    ruser = -1
+    rhost = -1
+    rport = -1
+    user = -1
+
+    openSession = re.compile("pam_unix\((.*)\): session opened for user (.*) by (.*)\(uid=([0-9]+)\)").match(message)
+    closeSession = re.compile("pam_unix\((.*)\): session closed for user (.*)").match(message)
+    loginFail = re.compile("pam_unix\((.*)\): authentication failure; logname=(.*) uid=([0-9]+) euid=([0-9]+) tty=(.*) ruser=(.*) rhost=(.*)  user=(.*)").match(message)
+    passFail = re.compile("password check failed for user \((.*)\)").match(message)
+    remotePassFail = re.compile("Failed password for (.*) from (.*) port ([0-9]+) (.*)").match(message)
+    if (openSession):
+        warning = 0
+        info = openSession.groups()
+        process = info[0]
+        login = info[1]
+        uid = info[2]
+    elif closeSession:
+        warning = 0
+        info = closeSession.groups()
+        process = info[0]
+        login = info[1]
+    elif loginFail:
+        warning = 1
+        info = loginFail.groups()
+        process = info[0]
+        login = info[1]
+        uid = info[2]
+        euid = info[3]
+        tty = info[4]
+        ruser = info[5]
+        rhost = info[6]
+        user = info[7]
+    elif passFail:
+        warning = 1
+        info = passFail.groups()
+        login = info[0]
+    elif remotePassFail:
+        warning = 1
+        info = remotePassFail.groups()
+        login = info[0]
+        rhost = info[1]
+        rport = info[2]
+        process = info[3]
+
+    return (warning, process, login, uid, euid, tty, ruser, rhost, rport, user)
+
 def switch(msg):
     message = string.split(msg, '|', 1)
     if str(message[0]).strip() == "security":
         time, hostname, message = getDetails(message[1])
         message = message[message.index(' ')+1:]
         process, pid, message = getProcessInfo(message)
-        print time
-        print hostname
-        print message
+        warning, process, login, uid, euid, tty, ruser, rhost, rport, user = extractSecurity(message)
+        print warning
         print process
-        print pid
+        print login
+        print uid
+        print euid
+        print tty
+        print ruser
+        print rhost
+        print rport
+        print user
+
     elif str(message[0]).strip() == "messages":
         time, hostname, message = getDetails(message[1])
         message = message[message.index(' ')+1:]
         process, pid, message = getProcessInfo(message)
-        print time
-        print hostname
-        print message
-        print process
-        print pid
 
 while True:
     msg = socket.recv()
